efc9f5ab4ced36daf733814c0d688dd532cea819
Gabriel Houle <ghoule636@gmail.com>
2026-02-08 20:48:00 -0800

    customer nitpicks

--- WeddingSite/obj/Release/net9.0/WeddingSite.AssemblyInfo.cs
+++ WeddingSite/obj/Release/net9.0/WeddingSite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeddingSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7862ac6ed4e7384929c7ebbe36e475eef012f719")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+149f1387b9b6b0cdb9927c01838c86412ac69ea2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeddingSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeddingSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
